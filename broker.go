be268775c473605dd7d5571483c694e40a78083c
Matthew Lavine <matt@matthewlavine.net>
2021-01-04 18:48:39 -0500

    Move shutdown handling to sub-package

--- broker.go
+++ broker.go
@@ -4,6 +4,7 @@ import (
 	"BrokerBot/cryptolib"
 	"BrokerBot/messageutil"
 	"BrokerBot/secrets"
+	"BrokerBot/shutdownutil"
 	"BrokerBot/stocklib"
 	"context"
 	"flag"
@@ -12,9 +13,7 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/Finnhub-Stock-API/finnhub-go"
@@ -93,19 +92,7 @@ func main() {
 		log.Fatalf("failed to open Discord client: %v", err)
 	}
 
-	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
-	go func() {
-		s := <-sigc
-		log.Printf("Caught %v, shutting down connection to Discord.", s)
-		discordClient.Close()
-		log.Printf("DiscordBot shutting down gracefully.")
-		os.Exit(0)
-	}()
+	shutdownutil.AddShutdownHooks(discordClient)
 
 	http.HandleFunc("/", handleDefaultPort)
 
---
+++ shutdownutil/shutdownutil.go
@@ -0,0 +1,29 @@
+package shutdownutil
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// AddShutdownHooks registers a shutdown handler to be run upon receiving various OS signals.
+func AddShutdownHooks(s *discordgo.Session) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go shutdownHandler(sigChan, s)
+}
+
+func shutdownHandler(sigChan chan os.Signal, s *discordgo.Session) {
+	sig := <-sigChan
+	log.Printf("Caught %v, shutting down connection to Discord.", sig)
+	s.Close()
+	log.Printf("DiscordBot shutting down gracefully.")
+	os.Exit(0)
+}

9c80ad1868b7c7809269c9bd066ab8635534428c
Matthew Lavine <matt@matthewlavine.net>
2021-01-10 10:39:23 -0500

    Minor readability changes for shutdownlib

--- shutdownlib/shutdownlib.go
+++ shutdownlib/shutdownlib.go
@@ -9,20 +9,23 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+var gracefulShutdownSignals = []os.Signal{
+	syscall.SIGHUP,
+	syscall.SIGINT,
+	syscall.SIGTERM,
+	syscall.SIGQUIT,
+}
+
 // AddShutdownHooks registers a shutdown handler to be run upon receiving various OS signals.
 func AddShutdownHooks(s *discordgo.Session) {
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
+	signal.Notify(sigChan, gracefulShutdownSignals...)
 	go shutdownHandler(sigChan, s)
 }
 
 func shutdownHandler(sigChan chan os.Signal, s *discordgo.Session) {
 	sig := <-sigChan
-	log.Printf("Caught %v, shutting down connection to Discord.", sig)
+	log.Printf("Caught signal %q, shutting down connection to Discord.", sig)
 	s.Close()
 	log.Printf("DiscordBot shutting down gracefully.")
 	os.Exit(0)
